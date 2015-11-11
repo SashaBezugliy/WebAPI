17db263c3fbcc809c027a1e28f8dd7bfa309d1f4
SashaBezugliy <sasha.bezugliy@gmail.com>
2015-11-11 23:33:44 +0200

    First Commit

---
+++ DataAccess/DAL/ProductLocationDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAL
+{
+    public class ProductLocationDAL
+    {
+        public List<ProductLocation> GetProductLocations(int supermarketId)
+        {
+            using (var sf = new SimplyFindEntities())
+            {
+                return sf.ProductLocation.Where(pl => pl.SupermarketId == supermarketId).ToList();
+            }
+        }
+    }
+}
