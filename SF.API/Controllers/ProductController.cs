f335295ffb23951a924f5f54f89a83d6cfb33f40
Sasha Bezuglyi <Oleksandr_Bezuglyi@epam.com>
2016-03-29 20:13:48 +0300

    Auth in API initial version. not working, not deployed

---
+++ SF.API/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Http;
+using DataAccess.DAL;
+using Newtonsoft.Json;
+
+namespace SF.API.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private ProductLocationDAL _dal = new ProductLocationDAL();
+
+        // GET api/product/5
+        //[EnableCors(origins: "*", headers: "*", methods: "*")]
+        public string Get(int id)
+        {
+            return JsonConvert.SerializeObject(_dal.GetProductLocations(id));
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SF.API/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using SF.API;
+using SF.API.Providers;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace SF.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+
+    }
+}
